a826cdc3ec5e163a45ec48047626957998edf843
Chris Deacy <chrisdothtml@gmail.com>
2016-11-12 12:27:55 -0600

    adding custom upload to plugins module

--- gulpfile.js/lib/plugins.js
+++ gulpfile.js/lib/plugins.js
@@ -1,15 +1,35 @@
-/*
- * Using this module so plugins are loaded once then
- * cached
- */
 'use strict'
 
-const plugins = require('gulp-load-plugins')({
-  camelize: true
-})
+const config = require('../config.js')
+const plugins = require('gulp-load-plugins')({ camelize: true })
+const shopifyUpload = plugins.shopifyUpload
+
+/**
+ * Wrapper for gulp-shopify-upload that fills in shopify
+ * API info
+ */
+function upload (basePath = './') {
+  const {
+    api_key,
+    api_password,
+    store_url
+  } = config.shopify
+
+  return shopifyUpload(
+    api_key,
+    api_password,
+    store_url,
+    null,
+    { basePath }
+  )
+}
 
 // rename cache plugin
 plugins.cache = plugins.cached
 delete plugins.cached
+
+// replace shopifyUpload plugin
+plugins.upload = upload
+delete plugins.shopifyUpload
 
 module.exports = plugins
