ff8a3b33aa9f606a376b4ef3dfdede2a8e631834
Chris Deacy <chrisdothtml@gmail.com>
2016-12-07 02:17:27 -0600

    Adding stock indicator UI

--- src/assets/js/product-stock.js
+++ src/assets/js/product-stock.js
@@ -10,27 +10,62 @@
   }
 
   /**
-   * Updates the indicator based on the provided variant
+   * Updates the indicator according to the provided status
+   */
+  function applyStatus (status) {
+    var $indicator = $('#stock-indicator')
+    var estimate = ''
+    var title = ''
+
+    switch (status) {
+      case 'in':
+        title = 'In stock'
+        break
+      case 'soon':
+        title = 'In stock soon. Pre-order now'
+        break
+      case 'out':
+        title = 'Out of stock'
+    }
+
+    if (status === 'soon' && store.estimate) {
+      estimate = `<span class="separator">|</span><span class="estimate">Estimated ${store.estimate}</span>`
+    }
+
+    $indicator
+      .removeClass('in')
+      .removeClass('soon')
+      .removeClass('out')
+      .addClass(status)
+      .html(`<i class="fa"></i><span class="status">${title}</span>${estimate}`)
+  }
+
+  /**
+   * Refreshes the indicator
    */
   function update (variant) {
-    var $indicator = $('#stock-indicator')
     var status
 
     if (variant.available) {
       if (variant.inventory_quantity > 0) {
-        status = 'in stock'
+        status = 'in'
       } else {
-        status = 'in stock soon'
-
-        if (store.estimate) {
-          status += '; ' + store.estimate
-        }
+        status = 'soon'
       }
     } else {
-      status = 'out of stock'
+      status = 'out'
     }
 
-    $indicator.text(status)
+    applyStatus(status)
+  }
+
+  /**
+   * @returns {string}
+   */
+  function convertEstimate (est) {
+    return est
+      .replace(/(\d+)w/, "$1 weeks") // eslint-disable-line quotes
+      .replace(/(\d+)m/, "$1 months") // eslint-disable-line quotes
   }
 
   /**
@@ -50,7 +85,7 @@
       })
 
     if (estimate) {
-      store.estimate = estimate
+      store.estimate = convertEstimate(estimate)
     }
   }
 
